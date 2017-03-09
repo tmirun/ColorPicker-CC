872c6de6b6be4c4f5397ade13e0524f9fe4dbbd4
tmirun <tmirun.app@gmail.com>
2017-03-09 18:42:17 +0100

    change server to browser-sync && add plumber to dont stop watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,40 @@ const gulp = require("gulp");
 const gulpLoadPlugins = require("gulp-load-plugins");
 const $ = gulpLoadPlugins();
 const chalk = require('chalk');
+const browserSync = require('browser-sync').create();
+
+var bsConfig = require('./bs-config.json');
+
+const paths = {
+  scripts: ["src/*.js"],
+}
+
+// build
+gulp.task('build', function() {
+  // Minify and copy all JavaScript (except vendor scripts)
+  // with sourcemaps all the way down
+  return gulp.src(paths.scripts)
+    .pipe($.plumber())
+    .pipe($.sourcemaps.init())
+      .pipe($.babel({
+          presets: ['es2015']
+      }))
+      .on("error", $.util.log)
+      .pipe($.uglify())
+      .pipe($.concat("colorpicker-cc.min.js"))
+    .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('./dist'));
+});
+
+//watch
+gulp.task('watch', function() {
+  gulp.watch(paths.scripts, ['build'])
+  .on("change", browserSync.reload);
+});
+
+//serve
+gulp.task('serve', function(){
+    browserSync.init(bsConfig);
+});
+
+gulp.task("default", ["build", "serve", "watch"]);
