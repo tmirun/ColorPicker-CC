57bd8925e6b9ab7d4280e19c80ccf4c00ce1c0b7
tmirun <tmirun.app@gmail.com>
2017-03-13 13:55:05 +0100

    remove import snap svg in app

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-const Snap = require(`imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg.js`);
 import ColorPickerRgb from "./colorpicker.rgb";
 
 var rgb = new ColorPickerRgb("#paper-rgb");
