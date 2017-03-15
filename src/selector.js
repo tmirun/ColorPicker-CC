185f3bc693ca2c39120db627fe84b9c1a8ea56ee
tmirun <tmirun.app@gmail.com>
2017-03-15 17:00:38 +0100

    clear code

--- src/selector.js
+++ src/selector.js
@@ -1,25 +1,20 @@
 export default class Selector{
+
   constructor(paper, params, defaultParams = {}){
+    
     if(!paper){
       throw new Error('Selector error: shoult pass snap paper object');
     }
+
+    //define varibales;
     this.paper = paper;
     this.group = this.paper.g();
-    this.value;
     this._value = 0;
+    this.selector;
 
+    //parsing params
     Object.assign(defaultParams, params);
     Object.assign(this, defaultParams);
-  }
-  getElementAbsolutePosition(element){
-    var rect = element.getBoundingClientRect();
-    return {
-      x: rect.left + window.scrollX,
-      y: rect.top + window.scrollY
-    }
-  }
-  onchange(value){
-    console.log(value);
   }
 
   get value(){
@@ -29,5 +24,17 @@ export default class Selector{
   set value(value){
     this._value = value;
     this.onchange(value);
+  }
+
+  getElementAbsolutePosition(element){
+    var rect = element.getBoundingClientRect();
+    return {
+      x: rect.left + window.scrollX,
+      y: rect.top + window.scrollY
+    }
+  }
+
+  onchange(value){
+    console.log(value);
   }
 }
